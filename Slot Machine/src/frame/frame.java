49497a795d7c4a35c0d65f9c88c00efe928681b8
Alexander Thomas <a.thomas.kurniawan@gmail.com>
2018-03-12 10:47:57 +0700

    Game Over
    Slot Machine cannot be rerolled when "Jackpot" is occured

--- Slot Machine/src/frame/frame.java
+++ Slot Machine/src/frame/frame.java
@@ -147,19 +147,19 @@ public class frame extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        if(!slot1.stopped)slot1.stopped = true;else slot1.stopped =false;
+        if(!slot1.stopped)slot1.stopped = true;else if(!jLabel1.getText().equals("Jackpot")) slot1.stopped =false;
         checker1.check=true;
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        if(!slot2.stopped)slot2.stopped = true;else slot2.stopped =false;
+        if(!slot2.stopped)slot2.stopped = true;else if(!jLabel1.getText().equals("Jackpot")) slot2.stopped =false;
         checker1.check=true;
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        if(!slot3.stopped)slot3.stopped = true;else slot3.stopped =false;
+        if(!slot3.stopped)slot3.stopped = true;else if(!jLabel1.getText().equals("Jackpot")) slot3.stopped =false;
          checker1.check=true;
     }//GEN-LAST:event_jButton3ActionPerformed
 
